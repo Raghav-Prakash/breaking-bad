45cc8f55e20c27227fd63830d5e01b1e1a1d40c7
Raghav-Prakash <raghrav2995@gmail.com>
2020-09-05 17:17:59 -0600

    Add lodash and css for switching tabs selection

--- src/app/tabset/tabset.component.ts
+++ src/app/tabset/tabset.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-tabset',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabset.component.less'],
 })
 export class TabsetComponent {
+  /**
+   * Map containing active flags for each tab. Only one tab can be active at
+   * a time, to indicate selection. By default, the "Characters" tab is
+   * selected.
+   */
+  tabsSelect: { [tab: string]: boolean } = {
+    Characters: true,
+    Quotes: false,
+    Deaths: false,
+  };
+
+  /**
+   * Set the selected tab to be active and all other previously active tabs
+   * to be inactive.
+   * @param tab The tab selected.
+   */
+  onTabsSelect(tab: ElementRef) {
+    this.tabsSelect = _.mapValues(this.tabsSelect, () => false);
+    this.tabsSelect[(tab as any).innerText] = true;
+  }
 }
