c7e0a87b0757c205809f92f21f891c02406a11a1
Raghav-Prakash <raghrav2995@gmail.com>
2020-09-05 21:41:35 -0600

    Remove the necessity for app prefix for selectors

---
+++ src/app/characters/characters.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'characters-list',
+  templateUrl: './characters.component.ts',
+  styleUrls: ['./characters.component.less'],
+})
+export class CharactersComponent {
+}
