f87a4a3587289131e235e326e53734f6294df86b
Raghav-Prakash <raghrav2995@gmail.com>
2021-06-05 16:57:02 -0600

    Add sidemenu styles. Use mat-list and mat-divider.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 // Components
 import { NavigatePagesComponent } from 'app/shared/navigate-pages/navigate-pages.component';
@@ -17,6 +19,8 @@ import { SideMenuComponent } from 'app/shared/side-menu/side-menu.component';
     BrowserModule,
     PaginationModule.forRoot(),
     MatSidenavModule,
+    MatListModule,
+    MatDividerModule,
     HttpClientModule,
   ],
   exports: [
